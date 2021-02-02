389d3cf6e1121f2e2fe5492fded18df4ca1d58c2
杨卫生 <985089028@qq.com>
2021-02-02 15:23:47 +0800

    测试IDEA中使用Git

---
+++ src/com/atguigu/test/Test.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @Author: yws
+ * @Date: 2021/2/2 15:21
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
