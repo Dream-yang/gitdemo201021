0f25821f96deb7c85551d7da06fce49189c47fef
杨卫生 <985089028@qq.com>
2021-02-02 15:58:13 +0800

    dev中添加了22222

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -8,5 +8,6 @@ package com.atguigu.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("111111111dev");
+        System.out.println("2222222222222");
     }
 }
